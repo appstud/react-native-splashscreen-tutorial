3ed759decda0c9054ca5337de25c63b88cf9f84e
Oleksandr Naumkin <aymkin@gmail.com>
2020-08-22 10:16:31 +0300

    fix bug: notifications are not triggered
    that happens if tap on a notification from quit or background mode
    https://github.com/invertase/react-native-firebase/issues/3469#issuecomment-614990736

--- android/app/src/main/java/com/rn_splashscreen_tutorial/SplashActivity.java
+++ android/app/src/main/java/com/rn_splashscreen_tutorial/SplashActivity.java
@@ -7,10 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        try {
+      super.onCreate(savedInstanceState);
+      Intent intent = new Intent(this, MainActivity.class);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+      // Pass along FCM messages/notifications etc.
+      Bundle extras = getIntent().getExtras();
+      if (extras != null) {
+        intent.putExtras(extras);
+      }
+      startActivity(intent);
+      finish();
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
     }
 }
